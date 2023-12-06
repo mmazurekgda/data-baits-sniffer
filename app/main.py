0dd6dce02880c1fa1b418ab1c5f9e7939e5654ec
Michał Mazurek <mmazurekgda@gmail.com>
2023-12-06 09:38:08 +0100

    Try out KFP

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
-# from time import sleep
-# from datetime import datetime
-# import kfp
+from time import sleep
+from datetime import datetime
+import kfp
 
 if __name__ == "__main__":
     print("Welcome to the Data-Baits-Sniffer!")
-    # while True:
-    #     print("Checking for new experiments...")
-    #     print(kfp.Client().list_experiments())
-    #     print(f"Checked at: {datetime.now()}")
-    #     sleep(1)
+    while True:
+        print("Checking for new experiments...")
+        print(kfp.Client().list_experiments())
+        print(f"Checked at: {datetime.now()}")
+        sleep(60)
